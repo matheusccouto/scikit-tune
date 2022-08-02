181453db692815e8cc44ef72fd8b0805d835bd24
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-08-02 18:24:22 +0000

    add worfklows

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""Setup Python package."""
+
+import os
+import setuptools
+
+THIS_DIR = os.path.dirname(__file__)
+
+with open(os.path.join(THIS_DIR, "requirements.txt"), encoding="utf-8") as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="scikit-tune",
+    version="0.1.0",
+    author="Matheus Couto",
+    author_email="matheusccouto@gmail.com",
+    description="A friendly way to tune scikit-learn pipelines.",
+    packages=["sktune"],
+    install_requires=required,
+)
