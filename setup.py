11faa2e9c090b84e2709cef42e7255a84837d26c
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-08-09 14:38:19 +0000

    Include package long description

--- setup.py
+++ setup.py
@@ -8,12 +8,17 @@ THIS_DIR = os.path.dirname(__file__)
 with open(os.path.join(THIS_DIR, "requirements.txt"), encoding="utf-8") as f:
     required = f.read().splitlines()
 
+with open(os.path.join(THIS_DIR, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="scikit-tune",
     version="0.2.2",
     author="Matheus Couto",
     author_email="matheusccouto@gmail.com",
     description="A friendly way to tune scikit-learn pipelines.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["sktune"],
     install_requires=required,
 )
